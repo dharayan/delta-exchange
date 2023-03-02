766281eb3b321901c4bb43bfddd1a2c31c4aa0fb
Ayan Dhara <git@ayandhara.me>
2023-03-03 01:37:46 +0530

    added simple test

---
+++ test/index.ts
@@ -0,0 +1,15 @@
+import {DeltaExchangeClient, Resolution} from "../lib";
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const deltaExchangeClient = new DeltaExchangeClient()
+
+const main = async () => {
+  const data = await deltaExchangeClient.getOHLC("BTCUSDT", Resolution._3m, Date.now() - 1000 * 60 * 60, Date.now())
+  if (data?.length < 10) {
+    throw new Error("invalid data received")
+  }
+}
+
+main().then(() => console.log("test successfully completed"))
