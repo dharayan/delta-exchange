ffd406e39e1e99b15584b57bd54eb555e63c0fb4
Ayan Dhara <git@ayandhara.me>
2024-10-11 20:20:20 +0530

    test fix

--- test/index.ts
+++ test/index.ts
@@ -3,13 +3,13 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-const deltaExchangeClient = new DeltaExchangeClient()
+const deltaExchangeClient = new DeltaExchangeClient("a", "b")
 
 const main = async () => {
-  const data = await deltaExchangeClient.getOHLC("BTCUSDT", Resolution._3m, Date.now() - 1000 * 60 * 60, Date.now())
-  if (data?.length < 10) {
-    throw new Error("invalid data received")
-  }
+    const data = await deltaExchangeClient.getOHLC("BTCUSDT", Resolution._3m, Date.now() - 1000 * 60 * 60, Date.now())
+    if (data?.length < 10) {
+        throw new Error("invalid data received")
+    }
 }
 
 main().then(() => console.log("test successfully completed"))
